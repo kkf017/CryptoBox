0e997bc7d69b2cb1c6f85878e003c4cfa6f37f3c
kkf17 <favre.kathleen@orange.fr>
2024-05-07 15:28:06 +0200

    Updt ktp

--- CryptoBox/ktp/ktp.py
+++ CryptoBox/ktp/ktp.py
@@ -24,18 +24,6 @@ class DiffieHellman:
 
 class NeedhamSchroeder:
 	def __init__(self, A):
-		self.A = A
-		
-	def PrivateKey(self,)->Tuple[int]:
-		return self.A.PrivateKey()
-		
-	def PublicKey(self,)->Tuple[int]:
-		return self.A.PublicKey()
-	
-	def exchange(self, key:Tuple[int], *args:Tuple[str])->Tuple[int]:
-		pass
-	
-	def verification(self, key:Tuple[int], *args:Tuple[int])->Tuple[int]:
 		pass 
 		
 class EKE:
